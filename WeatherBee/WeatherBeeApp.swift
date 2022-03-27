7337dc7a70670ac34332f5c63da48f95198520fc
Anurag Ambuj <anuragambuj000@gmail.com>
2022-03-27 21:58:38 +0530

    Initial Commit

---
+++ WeatherBee/WeatherBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherBeeApp.swift
+//  WeatherBee
+//
+//  Created by Anurag Ambuj on 27/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
